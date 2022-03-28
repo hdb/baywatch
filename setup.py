e6b634b396c9870e21525d14884fd372b9ad2374
Hudson Bailey <hudsondiggsbailey@gmail.com>
2022-03-27 21:34:05 -0500

    refactor dependencies to avoid pypi direct url error

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'requests>=2.27.1',
         'textual==0.1.17',
         'textual_inputs==0.2.5',
-        'ck-widgets-lv @ git+ssh://git@github.com/Cvaniak/TextualListViewUnofficial.git@52ea0f2f6bdcdc05961a1b64f538b5ef4cf1e852',
+        # 'ck-widgets-lv @ git+https://github.com/Cvaniak/TextualListViewUnofficial.git@52ea0f2f6bdcdc05961a1b64f538b5ef4cf1e852',
         'pyfiglet==0.8.post1',
         'transmission-rpc==3.3.0',
         'pyperclip==1.8.2',
